d5f5ab4515e2ec0e74635be472f227b4aee349e9
jeanSaid <yansaid21@gmail.com>
2023-10-24 21:28:02 -0500

    listo el crud inicial

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,7 +2,7 @@ const express = require("express");
 const UserController = require("../controllers/user");
 const api = express.Router();
 
-api.get("/user/me", UserController.getMe);
+//api.get("/user/me", UserController.getMe);
 api.get("/users", UserController.getUsers);
 api.post("/user", UserController.createUser);
 api.patch(
