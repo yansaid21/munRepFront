20c30f26e8a907acf73f45f6363c89b9bddefcb3
jeanSaid <yansaid21@gmail.com>
2023-11-26 20:20:26 -0500

    image items works

--- controllers/item.js
+++ controllers/item.js
@@ -1,6 +1,8 @@
 const bcrypt = require("bcryptjs");
 const Item = require("../models/item");
 const image = require("../utils/image");
+const fs = require("fs");
+const path = require("path");
 
 async function getItems(req, res) {
   try {
@@ -13,7 +15,31 @@ async function getItems(req, res) {
       response = await Item.find({ Active });
     }
 
-    res.status(200).send(response);
+    // Procesar cada item para convertir la imagen en Base64
+    const itemsWithBase64 = await Promise.all(response.map(async (item) => {
+      // Verificar si la propiedad 'Photo' existe y no es null o undefined
+      if (item.Photo) {
+        // Construir la ruta completa a la imagen
+        const photoPath = path.join(__dirname,'../', item.Photo);
+
+        try {
+          // Leer la imagen y convertirla en Base64
+          const photoData = fs.readFileSync(photoPath, { encoding: 'base64' });
+          item.Photo=photoData
+          // Agregar la propiedad 'photoBase64' al item
+          return item
+        } catch (error) {
+          console.error(`Error al procesar la imagen para ${item._id}:`, error);
+          // En caso de error al procesar una imagen, devolver el item sin modificar
+          return item;
+        }
+      } else {
+        // Si no hay propiedad 'Photo', devolver el item sin modificar
+        return item;
+      }
+    }));
+
+    res.status(200).send(itemsWithBase64);
   } catch (error) {
     console.error("Error al obtener los items:", error);
     res.status(500).send({ msg: "Error interno del servidor" });
@@ -22,8 +48,12 @@ async function getItems(req, res) {
 
 async function createItem(req, res) {
   try {
-    const item = new Item({ ...req.body, Active: false, Showcase: false , Photo:null});
-
+    const item = new Item({
+      ...req.body,
+      Active: false,
+      Showcase: false,
+      Photo: null,
+    });
 
     const itemStored = await item.save();
     console.log("Se ha creado el item", item);
@@ -38,18 +68,20 @@ async function updateItem(req, res) {
   try {
     const { id } = req.params;
     const itemData = req.body;
-    console.log("req.body",req.body);
-    console.log("req.files",req.files);
-    console.log("req.files.Photo",req.files.Photo);
+    console.log("req.body", req.body);
+    console.log("req.files", req.files);
+    console.log("req.files.Photo", req.files.Photo);
 
     if (req.files?.Photo !== undefined) {
-      const imagePath = image.getFilePath(req.files.Photo);
-      console.log("IMAGEPATH DESPUES DEL REQFILES------>", imagePath);
-      itemData.Photo = imagePath;
-    }else{
+      // Obtén el nombre único del archivo generado por Multiparty
+      const imagepath = req.files.Photo.path
+      console.log("Nombre único del archivo --->", imagepath);
+
+      // Actualiza la propiedad Photo con el nombre único del archivo
+      itemData.Photo = imagepath;
+    } else {
       console.log(".PHOTO ES UNDEFINED");
     }
-    
 
     await Item.findByIdAndUpdate({ _id: id }, itemData).exec();
     console.log("item actualizado", itemData);
@@ -60,6 +92,7 @@ async function updateItem(req, res) {
     res.status(400).send({ msg: "Error al actualizar el item" });
   }
 }
+
 
 async function deleteItem(req, res) {
   try {
--- utils/image.js
+++ utils/image.js
@@ -5,6 +5,7 @@ const path = require("path");
 function getFilePath(photoFile) {
   const uploadDir = "uploads/item"; // Directorio donde se almacenan las imágenes
   const photoPath = path.join(uploadDir, photoFile.name); // Combina el directorio y el nombre del archivo
+  console.log("photoPath dentro de image",photoPath);
   return photoPath;
 }
 
